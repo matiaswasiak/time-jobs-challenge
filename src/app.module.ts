2443b3f7ece078f301106592bd78c9ceb00f9edf
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-04 02:56:36 -0300

    Connecting Nest with Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeatherModule } from './weather/weather.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [WeatherModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/time-jobs'),
+    WeatherModule,
+  ],
 })
 export class AppModule {}
