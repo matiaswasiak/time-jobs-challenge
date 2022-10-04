5f9d070440682be9d7440ec0d304fbf0a683bb46
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-04 04:11:19 -0300

    Handling a specific error

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import {
+  Injectable,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateWeatherDto } from './dto/create-weather.dto';
@@ -12,11 +17,23 @@ export class WeatherService {
   ) {}
 
   async create(createWeatherDto: CreateWeatherDto): Promise<Weather> {
-    const { city, temperature } = createWeatherDto;
-    return await this.weatherModel.create({
-      city,
-      temperature,
-    });
+    createWeatherDto.city = createWeatherDto.city.toLowerCase();
+
+    try {
+      const weather = await this.weatherModel.create(createWeatherDto);
+      return weather;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `This city already exists in DB ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+
+      console.log(error);
+      throw new InternalServerErrorException(
+        `Can't create City - Check server logs`,
+      );
+    }
   }
 
   async findByCity(city: string): Promise<Weather> {
