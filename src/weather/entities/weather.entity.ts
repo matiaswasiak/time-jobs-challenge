c93b68d3355175f07114d75993a56e1eadcfc73d
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-04 03:06:40 -0300

    Creating schemas and models

--- src/weather/entities/weather.entity.ts
+++ src/weather/entities/weather.entity.ts
@@ -1,1 +1,25 @@
-export class Weather {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Weather extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  temperature: number;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  city: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  searchDate: Date;
+}
+
+export const WeatherSchema = SchemaFactory.createForClass(Weather);
--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,9 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { WeatherController } from './weather.controller';
+import { Weather, WeatherSchema } from './entities/weather.entity';
 
 @Module({
   controllers: [WeatherController],
   providers: [WeatherService],
+  imports: [
+    MongooseModule.forFeature([{ name: Weather.name, schema: WeatherSchema }]),
+  ],
 })
 export class WeatherModule {}
