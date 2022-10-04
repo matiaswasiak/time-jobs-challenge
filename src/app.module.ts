bac2b6a5cc4099b649eecf8e39b2e3e023c85437
Matias Wasiak <matiaswasiak@gmail.com>
2022-10-04 04:35:07 -0300

    Creating adapter for axios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WeatherModule } from './weather/weather.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/time-jobs'),
     WeatherModule,
+    CommonModule,
   ],
 })
 export class AppModule {}
